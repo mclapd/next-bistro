071e8435579ac345cff531f9d8237f75a9fac7fd
one_punch_man <ckim@banlaw.com>
2023-11-08 17:16:51 +1100

    team

--- app/components/Team.jsx
+++ app/components/Team.jsx
@@ -1,5 +1,60 @@
+"use client";
+
+import { teamData } from "../data";
+import { motion } from "framer-motion";
+import { fadeIn } from "../effects/variants";
+import Image from "next/image";
+
 const Team = () => {
-  return <div>Team</div>;
+  const {
+    pretitle,
+    title,
+    sub1,
+    sub2,
+    name,
+    occupation,
+    signatureImg,
+    chefImg,
+  } = teamData;
+
+  return (
+    <section className="relative top-96 z-10 lg:top-[480px] min-h-[720px]">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row lg:gap-x-[120px] items-center lg:items-start">
+          <motion.div
+            variants={fadeIn("down", "tween", 0.6, 1.6)}
+            initial="hidden"
+            whileInView={"show"}
+            className="flex-1 text-center lg:text-left lg:pt-16"
+          >
+            <div className="pretitle">{pretitle}</div>
+            <h2 className="h2 capitalize">{title}</h2>
+            <p className="mb-[60px]">{sub1}</p>
+            <p>{sub2}</p>
+            <div className="my-[26px]">
+              <div className="text-2xl capitalize font-semibold text-accent">
+                {name}
+              </div>
+              <div className="text-[15px] font-semibold capitalize text-grey/70">
+                {occupation}
+              </div>
+            </div>
+            <div className="flex justify-center lg:justify-start mb-6 lg:mb-0">
+              <Image src={signatureImg} width={174} height={41} alt="image" />
+            </div>
+          </motion.div>
+          <motion.div
+            variants={fadeIn("up", "tween", 0.7, 1.6)}
+            initial="hidden"
+            whileInView={"show"}
+            className="flex-1"
+          >
+            <Image src={chefImg} width={440} height={771} alt="image" />
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Team;
